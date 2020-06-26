fc1e4c47401003962e2e1b2b101997470ee052bf
coryortega <coryortega3@gmail.com>
2020-06-26 12:28:34 -0700

    implemented react router, refactored for page layouts

--- ecommerce/src/components/Header/index.js
+++ ecommerce/src/components/Header/index.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import './styles.scss';
-import Logo from './../../assets/logo-holder.png'
+import { Link } from 'react-router-dom';
+
+import Logo from './../../assets/logo-holder.png';
 
 const Header = props => {
     return(
         <header className="header">
             <div className="wrap">
                 <div className="logo">
-                    <img src={Logo} alt="Simple Logo"/>
+                    <Link to="/">
+                        <img src={Logo} alt="Simple Logo"/>
+                    </Link>
+
+                </div>
+                <div className="callToActions">
+                    <ul>
+                        <li>
+                            <Link to="/registration">
+                                Register
+                            </Link>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </header>
