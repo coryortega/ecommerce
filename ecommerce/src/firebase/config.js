43ccd37c8b43520b8d97b51dc8917971487654ba
coryortega <coryortega3@gmail.com>
2020-06-28 23:02:10 -0700

    working on registration form

---
+++ ecommerce/src/firebase/config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBUjvc5cVL1LDiP4gw1xvapsSYh1Zsr20k",
+    authDomain: "ecommerce-16a0d.firebaseapp.com",
+    databaseURL: "https://ecommerce-16a0d.firebaseio.com",
+    projectId: "ecommerce-16a0d",
+    storageBucket: "ecommerce-16a0d.appspot.com",
+    messagingSenderId: "56135922590",
+    appId: "1:56135922590:web:598455732ef503b98edc8f",
+    measurementId: "G-C1ZW9QJQ9Y"
+  };
